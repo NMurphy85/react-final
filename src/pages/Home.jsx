208a08303231b75af8417f40231c2745cf3e4d12
NMurphy85 <murphy.nathant@gmail.com>
2026-04-05 02:20:22 -0400

    fixing api issue/fixing movie cards dynamically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,31 +4,41 @@ import { useParams } from "react-router-dom";
 const API_KEY = "1989ac72";
 
 const Home = () => {
+  const [sortBy, setSortBy] = useState("")  
+  
   const { id } = useParams();
-  const [query, setQuery] = useState("super");
-  const [searchTerm, setSearchTerm] = useState([]);
-  useEffect(() => {
+  const [query, setQuery] = useState('super' || []);
+  const [movies, setMovies] = useState([]);
+ const [searchTerm, setSearchTerm] = useState([]);
+ const sortedMovies = [...movies].sort((a, b) => {
+if (sortBy === "LOW_TO_HIGH") {
+        sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+      }
+      if (sortBy === "HIGH_TO_LOW") {
+        sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
+      }
+      if (sortBy === "YEAR--LOW") {
+        sortedMovies.sort((a, b) => a.Year.localeCompare(b.Year));
+      }
+      if (sortBy === "YEAR--HIGH") {
+        sortedMovies.sort((a, b) => b.Year.localeCompare(a.Year));
+ }
+ })
+ 
+ 
+ useEffect(() => {
     async function getMovies(searchTerm) {
-      const { data } = await axios.get(
+      const {data} = await axios.get(
         `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
       );
-      setSearchTerm(data);
       console.log(data);
-      if (searchTerm === "LOW_TO_HIGH") {
-        data.Search.sort((a, b) => a.Title.localeCompare(b.Title));
-      }
-      if (searchTerm === "HIGH_TO_LOW") {
-        data.Search.sort((a, b) => b.Title.localeCompare(a.Title));
-      }
-      if (searchTerm === "YEAR--LOW") {
-        data.Search.sort((a, b) => a.Year.localeCompare(b.Year));
-      }
-      if (searchTerm === "YEAR--HIGH") {
-        data.Search.sort((a, b) => b.Year.localeCompare(a.Year));
-      }
+      setSearchTerm(data.Search || []);
+      
     }
     getMovies();
   }, []);
+// 
+  // setMovies(data.Search || []);
   return (
     <>
       <div className="container">
@@ -37,12 +47,17 @@ const Home = () => {
             <div className="user">
               <div className="user-card">
                 <div class="user-card">
-            <div class="user-card__container">
-                <h3>{data.Title}</h3>
-                <p><b>Year</b> {data.Year}</p>
-                <p><b>imdbID</b> {data.imdbID}</p>
-                <img src={data.Poster}/>
-            </div>
+            {searchTerm.map((movie) =>( 
+             
+              <div class="user-card__container">
+                <h3>{movie.Title}</h3>
+                <p><b>Year</b> {movie.Year}</p>
+                <p><b>imdbID</b> {movie.imdbID}</p>
+                <img src={movie.Poster}/>
+              </div>
+            ))}
+              
+            
             </div>
               </div>
             </div>
@@ -50,7 +65,5 @@ const Home = () => {
         </div>
       </div>
     </>
-  );
-};
-
-export default Home;
+  );}
+export default Home
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react"
 
 const Main = () => {
   const [searchTerm, setSearchTerm] = useState([])
-const [query, setQuery] = useState("")
+const [query, setQuery] = useState([])
+const [sortBy, setSortBy] = useState("")  
   return (
     <div>
       
@@ -11,7 +12,8 @@ const [query, setQuery] = useState("")
      <form 
      className="search-form" 
      id="searchForm">
-      <input value={query}
+      <input 
+      value={query}
       onChange={(e) => setQuery(e.target.value)}
       className="search-form__input" 
       type="text" 
@@ -26,8 +28,12 @@ const [query, setQuery] = useState("")
             </div>
         </div>
         <div className="filter__wrap">
-      <select id="filter" onChange={(e) => setQuery(e.target.value)} className="filter filter--bold">
-        <option className="filter--bold" defaultValue="" disabled selected>Sort</option>
+      <select id="filter"
+      onChange={(e) => setSortBy(e.target.value)}
+       className="filter filter--bold"
+       value={sortBy}
+       >
+        <option  value="">Sort</option>
         <option className="filter--bold" defaultValue="LOW_TO_HIGH">Search, Low to High</option>
         <option className="filter--bold" defaultValue="HIGH_TO_LOW">Search, High to Low</option>
         <option className="filter--bold" defaultValue="YEAR--LOW">Year, Low to High</option>
