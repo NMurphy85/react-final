26600dd31a4071f5ce08070e85cf451e4e09459e
NMurphy85 <murphy.nathant@gmail.com>
2026-04-18 17:50:42 -0400

    fixing loading state errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,18 +42,19 @@ const Home = ({ query, setQuery, sortBy, setSortBy }) => {
             <div className="user-card">
               <div className="user-card__container">
                 {loading ? (
-                 
-                 [...Array(10)].map((_, index) => (
-                  
-                    <div className="movie__body--skeleton" key={index}>
+                 <div className="user-list">
+                 {[...Array(10)].map((_, index) => (
+                  <div className="user-card"key={index}>
+                    <div className="movie__body--skeleton" >
                       <h3 className="movie__title--skeleton"></h3>
                       <p className="movie__title--skeleton"></p>
                       <p className="movie__title--skeleton"></p>
+                      </div>
                       <img className="img__body--skeleton" alt="" />
                       </div>
                       
-                 ))
-
+                 ))}
+                 </div>
                 
                   
                 ) : (
